9c91bbfe071e84a8baa17bc835a59b69d1c70a03
Taylor Hetherington <taylorhetherington0@gmail.com>
2022-02-23 15:18:55 +0000

    Changed some print statements

--- Find_Words/Find_Words.cpp
+++ Find_Words/Find_Words.cpp
@@ -26,15 +26,13 @@ int main(int argc, char* argv[])
 
     wheels.ShowResults();
 
-    #if _DEBUG
-    chrono::duration<double, std::milli> dictLoad = t2 - t1;
+    chrono::duration<double, std::milli> dictLoad   = t2 - t1;
     chrono::duration<double, std::milli> wheelsLoad = t3 - t2;
-    chrono::duration<double, std::milli> generate = t4 - t3;
-    chrono::duration<double, std::milli> total = t4 - t1;
+    chrono::duration<double, std::milli> generate   = t4 - t3;
+    chrono::duration<double, std::milli> total      = t4 - t1;
 
-    cout << "Loaded Dictionary.txt in\t" << dictLoad.count() << "ms" << endl;
-    cout << "Loaded Wheels.txt in\t\t" << wheelsLoad.count() << "ms" << endl;
-    cout << "Generated Words in\t\t" << generate.count() << "ms" << endl;
-    cout << "Total Time Taken: \t\t" << total.count() << "ms" << endl;
-    #endif
+    cout << "Loaded Dictionary.txt in\t"    << dictLoad.count()     << "ms" << endl;
+    cout << "Loaded Wheels.txt in\t\t"      << wheelsLoad.count()   << "ms" << endl;
+    cout << "Generated Words in\t\t"        << generate.count()     << "ms" << endl;
+    cout << "Total Time Taken: \t\t"        << total.count()        << "ms" << endl;
 }
